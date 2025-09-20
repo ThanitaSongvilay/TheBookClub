7aaada56be3cadc38c06823c88e796b3f1010df9
Thanita Songvilay <thanitasongvilay@Saulos-MacBook-Pro.local>
2025-09-20 13:36:24 +0200

    homepage + header + footer

--- TheBookClubWebbApp/src/app/app.ts
+++ TheBookClubWebbApp/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home-page/home-page'; // Import your HomeComponent
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomeComponent], // Add HomeComponent here
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('TheBookClubWebbApp');
+  protected readonly title = signal('The Book Club');
 }
---
+++ TheBookClubWebbApp/src/app/home-page/home-page.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+
+@Component({
+  selector: 'app-home',
+  imports: [Header, Footer],
+  standalone: true,
+  templateUrl: './home-page.html',
+    styleUrl: './home-page.css'
+})
+export class HomeComponent {
+  protected readonly bookTitle = signal('The Great Gatsby');
+  protected readonly bookAuthor = signal('F. Scott Fitzgerald');
+  protected readonly bookDescription = signal(
+    'A classic novel of the Jazz Age, The Great Gatsby tells the story of the mysterious millionaire Jay Gatsby, his passion for the beautiful Daisy Buchanan, and the dark side of the American Dream.'
+  );
+}
